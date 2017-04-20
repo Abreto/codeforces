f85ba759951149a392fbad37a00499851e5dbb8e
Abreto Fu <m@abreto.net>
2017-04-20 16:47:43 +0800

    format

--- beta-round-13/13e.c
+++ beta-round-13/13e.c
@@ -6,15 +6,15 @@
 #define ISDIGIT(x) ((x>='0')&&(x<='9'))
 void readn(int *n)
 {
-int c=0;*n=0;
-for(;!ISDIGIT(c);c=getchar());
-for(;ISDIGIT(c);c=getchar())*n=(*n)*10+c-'0';
+    int c=0;*n=0;
+    for(;!ISDIGIT(c);c=getchar());
+    for(;ISDIGIT(c);c=getchar())*n=(*n)*10+c-'0';
 }
 void putn(int n)
 {
-int ns[16]={0,n%10},nd=1;
-while(n/=10) ns[++nd]=n%10;
-while(nd) putchar(ns[nd--]+'0');
+    int ns[16]={0,n%10},nd=1;
+    while(n/=10) ns[++nd]=n%10;
+    while(nd) putchar(ns[nd--]+'0');
 }
 
 int N, M;
